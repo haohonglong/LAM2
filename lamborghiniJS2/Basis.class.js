bf1e2fe0573c5291c787d7f55f20197f36a644a0
龙昊宏 <947923503@qq.com>
2017-07-18 15:25:16 +0800

    add

--- lamborghiniJS2/Basis.class.js
+++ lamborghiniJS2/Basis.class.js
@@ -1225,6 +1225,44 @@ if(!GRN_LHH){
 		}
 		return this;
 	};
+	// from http://javascript.crockford.com/inheritance.html
+	Function
+		.method('inherits', function (parent) {
+			this.prototype = new parent();
+			var d = {},
+				p = this.prototype;
+			this.prototype.constructor = parent;
+			this.method('uber', function uber(name) {
+				if (!(name in d)) {
+					d[name] = 0;
+				}
+				var f, r, t = d[name], v = parent.prototype;
+				if (t) {
+					while (t) {
+						v = v.constructor.prototype;
+						t -= 1;
+					}
+					f = v[name];
+				} else {
+					f = p[name];
+					if (f == this[name]) {
+						f = v[name];
+					}
+				}
+				d[name] += 1;
+				r = f.apply(this, Array.prototype.slice.apply(arguments, [1]));
+				d[name] -= 1;
+				return r;
+			});
+			return this;
+		})
+		.method('swiss', function (parent) {
+			for (var i = 1; i < arguments.length; i += 1) {
+				var name = arguments[i];
+				this.prototype[name] = parent.prototype[name];
+			}
+			return this;
+		});
 
 	/**
 	 *
