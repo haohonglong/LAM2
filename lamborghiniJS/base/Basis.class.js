bc95f8d3b9abe99502ea214295f75cc7b10557cb
龙昊宏 <947923503@qq.com>
2017-11-01 16:09:22 +0800

    fixed

--- lamborghiniJS/base/Basis.class.js
+++ lamborghiniJS/base/Basis.class.js
@@ -2180,8 +2180,9 @@ window[GRN_LHH].run([window],function(W,Config){
 	System.merge(System.Config.XHR || {},[Config.XHR]);
 	System.merge(System.Config || {},[Config]);
 	System.classPath = System.Config.getClassPath();
-	System.Public 	 = System.Config.Public;
-	System.each(System.Public,function(name){
+	System.Public 	 = System.Config.Public || System.createDict();
+	System.components 	 = System.Config.components || System.createDict();
+	System.each(System.merge({},[System.components,System.Public]),function(name){
 		if(name in System){return true;}
 		System[name] = this;
 	});
