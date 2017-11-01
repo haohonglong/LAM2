e7fa87c03eadea55f5a1f739838eb2db41a96aa6
龙昊宏 <947923503@qq.com>
2017-11-01 17:12:17 +0800

    fixed

--- lamborghiniJS/base/Object.class.js
+++ lamborghiniJS/base/Object.class.js
@@ -24,13 +24,11 @@ window[GRN_LHH].run([window],function(window,undefined){
 			__this__=this;
 			if(!(System.app instanceof System.Object)){
 				System.app=this;
-				System.wait(function(){
-					System.each(System.components,function(name,value){
-						if(name in System.app){return true;}
-						if(System.isFunction(value)){
-							System['app'][name] = value.call(System.app,System);
-						}
-					});
+				System.each(System.components,function(name,value){
+					if(name in System.app){return true;}
+					if(System.isFunction(value)){
+						System['app'][name] = value.call(System.app,System);
+					}
 				});
 			}
 			this._hashCode=Object.generate();
