0b06aa535d713cfe2e2fe130401853d484a2965e
haohonglong <947923503@qq.com>
2019-07-03 16:46:42 +0800

    modify

--- lamborghiniJS/base/System.js
+++ lamborghiniJS/base/System.js
@@ -1381,7 +1381,11 @@
 		'hash':function(hashLength) {
             hashLength = Number(hashLength);
 			if (!System.isset(hashLength) || !System.isNumeric(hashLength) || hashLength < 1) {return '0';}
-			var ar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+			var ar = [];
+			ar[0] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+			ar[1] =[ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+			ar[2] =['!','@','#','$','%','+','-','=='];
+			ar = ar[0].merge(ar[1]).merge(ar[2]);
 			var hs = [];
 			var hl = hashLength;
 			var al = ar.length;
