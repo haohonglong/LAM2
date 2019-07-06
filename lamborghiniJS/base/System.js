f497683a006e42f6f49f29aadc530947d707308c
haohonglong <947923503@qq.com>
2019-07-06 14:22:54 +0800

    过滤敏感字符

--- lamborghiniJS/base/System.js
+++ lamborghiniJS/base/System.js
@@ -1439,7 +1439,7 @@
 			ar[0] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 			ar[1] = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 			ar[2] = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-			ar[3] = code && System.isString(code) && code.split('') || [];
+			ar[3] = code && System.isString(code) && code.replace(/_/g,'').split('') || [];
 			// ar[3] = ['!','@','#','$','%','+','-','=='];
 			ar = ar[0].merge(ar[1]).merge(ar[2]).merge(ar[3]);
 			var hs = [];
