84aec90007d2b935bda034aaf217d463b729d88c
haohonglong <947923503@qq.com>
2020-05-06 19:17:09 +0800

    $1

--- gitauto.sh
+++ gitauto.sh
@@ -1,2 +1,7 @@
-git add -u && git commit -m 'modify' && git push origin develop \
+#!/bin/bash
+
+if [ $1 ]; then
+    git add -u && git commit -m '$1'
+fi
+git push origin develop \
 && git checkout master && git merge develop && git push origin master && git checkout develop
