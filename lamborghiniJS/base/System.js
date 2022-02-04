47b75d5c42f2f96efcc2c2cba0334d12e57d2b88
龙昊宏 <947923503@qq.com>
2022-02-04 13:45:49 +0800

    修改版本号

--- lamborghiniJS/base/System.js
+++ lamborghiniJS/base/System.js
@@ -48,7 +48,7 @@
 })(typeof global !== 'undefined' ? global : this,function(global,namespace,undefined){
 	'use strict';
 // Used for trimming whitespace
-	var VERSION="v2.1.5",
+	var VERSION="v2.1.6",
 		Interface={},
 		System={},
 		once=true,
